044bd1eccee1b9770d298a92d00cd1ae701eb402
Aline Bellozo <alinebellozo@gmail.com>
2023-04-03 19:56:02 +0000

    Modified _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 // tells the Express app to use EJS as its templating engine:
 app.set("view engine", "ejs");
+
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,18 +24,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -70,8 +78,9 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
